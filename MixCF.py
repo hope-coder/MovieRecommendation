305862d764e4ba5d06f9eb002023ad5a2e021044
hope-coder <1765463091@qq.com>
2020-06-09 16:41:09 +0800

    22222

--- MixCF.py
+++ MixCF.py
@@ -3,7 +3,178 @@ import random
 import math
 from operator import itemgetter
 
+
 class MixCF():
 
     def __init__(self):
         self.n_sim_user = 200
+        self.n_sim_movie = 200
+        self.n_rec_movie = 2000
+
+        self.trainSet = {}
+        self.testSet = {}
+
+        self.user_sim_matrix = {}
+        self.movie_sim_matrix = {}
+        self.movie_popular = {}
+        self.movie_count = 0
+        print('Similar user number = %d' % self.n_sim_user)
+        print('Similar movie number = %d' % self.n_sim_movie)
+        print('Recommneded movie number = %d' % self.n_rec_movie)
+
+    def get_dataset(self, filename, pivot=0.85):
+        trainSet_len = 0
+        testSet_len = 0
+        for line in self.load_file(filename):
+            user, movie, rating, timestamp = line.split('::')
+            if random.random() < pivot:
+                self.trainSet.setdefault(user, {})
+                self.trainSet[user][movie] = rating
+                trainSet_len += 1
+            else:
+                self.testSet.setdefault(user, {})
+                self.testSet[user][movie] = rating
+                testSet_len += 1
+        print('Split trainingSet and testSet success!')
+        print('TrainSet = %s' % trainSet_len)
+        print('TestSet = %s' % testSet_len)
+
+    def load_file(self, filename):
+        with open(filename, 'r') as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    continue
+                yield line.strip('\r\n')
+        print('Load %s success!' % filename)
+
+    def calc_user_sim(self):
+
+        # key = movieID, value = list of userIDs who have seen this movie
+        print('Building movie-user table ...')
+        movie_user = {}
+        for user, movies in self.trainSet.items():
+            for movie in movies:
+                if movie not in movie_user:
+                    movie_user[movie] = set()
+                movie_user[movie].add(user)
+        print('Build movie-user table success!')
+
+        self.movie_count = len(movie_user)
+        print('Total movie number = %d' % self.movie_count)
+
+        print('Build user co-rated movies matrix ...')
+        for movie, users in movie_user.items():
+            for u in users:
+                for v in users:
+                    if u == v:
+                        continue
+                    self.user_sim_matrix.setdefault(u, {})
+                    self.user_sim_matrix[u].setdefault(v, 0)
+                    self.user_sim_matrix[u][v] += 1
+        print('Build user co-rated movies matrix success!')
+
+        print('Calculating user similarity matrix ...')
+        for u, related_users in self.user_sim_matrix.items():
+            for v, count in related_users.items():
+                self.user_sim_matrix[u][v] = count / math.sqrt(len(self.trainSet[u]) * len(self.trainSet[v]))
+        print('Calculate user similarity matrix success!')
+
+    def calc_movie_sim(self):
+        for user, movies in self.trainSet.items():
+            for movie in movies:
+                if movie not in self.movie_popular:
+                    self.movie_popular[movie] = 0
+                self.movie_popular[movie] += 1
+
+        self.movie_count = len(self.movie_popular)
+        print("Total movie number = %d" % self.movie_count)
+
+        print('Build user co-rated movies matrix ...')
+        for user, movies in self.trainSet.items():
+            for m1 in movies:
+                for m2 in movies:
+                    if m1 == m2:
+                        continue
+                    self.movie_sim_matrix.setdefault(m1, {})
+                    self.movie_sim_matrix[m1].setdefault(m2, 0)
+                    self.movie_sim_matrix[m1][m2] += 1
+        print("Build co-rated users matrix success!")
+
+        print("Calculating movie similarity matrix ...")
+        for m1, related_movies in self.movie_sim_matrix.items():
+            for m2, count in related_movies.items():
+                if self.movie_popular[m1] == 0 or self.movie_popular[m2] == 0:
+                    self.movie_sim_matrix[m1][m2] = 0
+                else:
+                    self.movie_sim_matrix[m1][m2] = count / math.sqrt(self.movie_popular[m1] * self.movie_popular[m2])
+        print('Calculate movie similarity matrix success!')
+
+    def recommendByItem(self, user):
+        K = self.n_sim_movie
+        N = self.n_rec_movie
+        rank = {}
+        watched_movies = self.trainSet[user]
+
+        for movie, rating in watched_movies.items():
+            for related_movie, w in sorted(self.movie_sim_matrix[movie].items(), key=itemgetter(1), reverse=True)[:K]:
+                if related_movie in watched_movies:
+                    continue
+                rank.setdefault(related_movie, 0)
+                rank[related_movie] += w * float(rating)
+        return sorted(rank.items(), key=itemgetter(1), reverse=True)[:N]
+
+    def recommendByUser(self, user):
+        K = self.n_sim_user
+        N = self.n_rec_movie
+        rank = {}
+        watched_movies = self.trainSet[user]
+
+        # v=similar user, wuv=similar factor
+        for v, wuv in sorted(self.user_sim_matrix[user].items(), key=itemgetter(1), reverse=True)[0:K]:
+            for movie in self.trainSet[v]:
+                if movie in watched_movies:
+                    continue
+                rank.setdefault(movie, 0)
+                rank[movie] += wuv
+        return sorted(rank.items(), key=itemgetter(1), reverse=True)[0:N]
+
+    def evaluate(self):
+        print("Evaluation start ....")
+        N = self.n_rec_movie
+
+        hit = 0
+        rec_count = 0
+        test_count = 0
+
+        all_rec_movies = set()
+        for i, user, in enumerate(self.trainSet):
+            test_movies = self.testSet.get(user, {})
+            rec_movies_user = self.recommendByUser(user)
+            rec_movies_item = self.recommendByItem(user)
+            rec_movies = []
+            for movie, w in rec_movies_user:
+                for moviee, ww in rec_movies_item:
+                    if movie == moviee:
+                        rec_movies.append(movie)
+
+            for movie in rec_movies:
+                if movie in test_movies:
+                    hit += 1
+                all_rec_movies.add(movie)
+            if i % 1000 == 0:
+                print("num:{}\n".format(i))
+            rec_count += N
+            test_count += len(test_movies)
+        precision = hit / (1.0 * rec_count)
+        recall = hit / (1.0 * test_count)
+        coverage = len(all_rec_movies) / (1.0 * self.movie_count)
+        print('precisioin=%.4f\trecall=%.4f\tcoverage=%.4f' % (precision, recall, coverage))
+
+
+if __name__ == '__main__':
+    rating_file = './ml-1m/ratings.dat'
+    mixCF = MixCF()
+    mixCF.get_dataset(rating_file)
+    mixCF.calc_user_sim()
+    mixCF.calc_movie_sim()
+    mixCF.evaluate()
